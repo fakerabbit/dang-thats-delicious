4b6a49fb0a9966ef2daf99418c25baac88ce1635
Mirko <mirkoj@gmail.com>
2017-11-22 23:52:07 -0500

    Created hello pug template

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,13 @@ const router = express.Router();
 // Do work here
 router.get('/', (req, res) => {
   //res.send('Hey! It works!');
-  const jsonObj = { name: 'DJ', age: 100, cool: true };
+  //const jsonObj = { name: 'DJ', age: 100, cool: true };
   //res.json(jsonObj);
-  res.json(req.query);
+  //res.json(req.query);
+  res.render('hello', {
+    name: 'John',
+    dog: req.query.dog,
+  });
 });
 
 router.get('/reverse/:name', (req, res) => {
