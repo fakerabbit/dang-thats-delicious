e173f4456f1890f03cd1d1ab4e9edc5c4cd9901f
Mirko <mirkoj@gmail.com>
2017-11-22 23:25:43 -0500

    added sample reverse route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  //res.send('Hey! It works!');
+  const jsonObj = { name: 'DJ', age: 100, cool: true };
+  //res.json(jsonObj);
+  res.json(req.query);
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
