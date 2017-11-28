a8a52600c92f8e7726d89796d67689b18822bf1e
Mirko <mirkoj@gmail.com>
2017-11-28 13:25:07 -0500

    Set up MVC implementation

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  //res.send('Hey! It works!');
-  //const jsonObj = { name: 'DJ', age: 100, cool: true };
-  //res.json(jsonObj);
-  //res.json(req.query);
-  res.render('hello', {
-    name: 'John',
-    dog: req.query.dog,
-  });
-});
-
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-});
+router.get('/', storeController.homePage);
 
 module.exports = router;
